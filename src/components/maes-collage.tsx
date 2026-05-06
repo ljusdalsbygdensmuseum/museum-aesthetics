6751dbbdde766b7eb32a67db41a03e62fe09898c
Ina Eklund <theartflower@gmail.com>
2026-05-06 10:23:07 +0200

    setup resize observer and moved generating img to a separate function

--- src/components/maes-collage.tsx
+++ src/components/maes-collage.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useRef, useState, useEffect } from 'react'
 
 import type {
 	CollageImage,
@@ -12,89 +12,126 @@ interface Props {
 
 export function MAESCollage({ background, imgs }: Props) {
 	const [positions, setPositions] = useState<CollageImageTransform[]>([])
+	const containerRef = useRef(null)
+	const [isWide, setIsWide] = useState(false)
 
 	const rotationRange = 60
 
-	const displayImgs = imgs.map((item, index) => {
-		if (!positions[index]) {
-			setPositions((oldPositions) => {
-				const newPositions = oldPositions.concat([])
+	function theImages() {
+		const theImgs = imgs.map((item, index) => {
+			if (!positions[index]) {
+				setPositions((oldPositions) => {
+					const newPositions = oldPositions.concat([])
 
-				let top = Math.random()
-				let left = Math.random()
+					let top = Math.random()
+					let left = Math.random()
 
-				//reroll position if it is in the middle (top > 0.2 && top < 0.8 && left > 0.2 && left < 0.8)
-				//side pref only works with bottom top right at the moment should be changed in future
-				// shuld be random throug log scaling and stuff
-				// temporary for banner only
-				while (
-					(top > 0.2 && top < 0.6 && left < 0.8) ||
-					left < 0.4 ||
+					//reroll position if it is in the middle (top > 0.2 && top < 0.8 && left > 0.2 && left < 0.8)
+					//side pref only works with bottom top right at the moment should be changed in future
+					// shuld be random throug log scaling and stuff
+					// temporary for banner only
+
+					/* ||
 					(left < 0.7 && item.isVertical) ||
 					(top < 0.7 && item.sidePref?.bottom) ||
 					(top > 0.3 && item.sidePref?.top) ||
-					(left < 0.8 && item.sidePref?.right)
-				) {
-					top = Math.random()
-					left = Math.random()
-				}
+					(left < 0.8 && item.sidePref?.right) */
 
-				const rotation = rotationRange
+					while (
+						(top > 0.2 && top < 0.6 && left < 0.8 && left > 0.2) ||
+						(left < 0.4 && !isWide)
+					) {
+						top = Math.random()
+						left = Math.random()
+					}
 
-				let rotationByPlace: number = (left * 2 - 1) * (top * 2 - 1) * rotation
+					const rotation = rotationRange
 
-				if (!item.isVertical) {
-					// invert rotation if image is horizontal
-					rotationByPlace = rotationByPlace * -1
+					let rotationByPlace: number =
+						(left * 2 - 1) * (top * 2 - 1) * rotation
 
-					// flip upside down if horizontal image is in the top of area
-					// if (top < 0.5) {
-					// 	rotationByPlace = rotationByPlace - 180
-					// }
-				}
+					if (!item.isVertical) {
+						// invert rotation if image is horizontal
+						rotationByPlace = rotationByPlace * -1
 
-				const fromCenter =
-					(left * 2 - 1 < 0 ? (left * 2 - 1) * -1 : left * 2 - 1) *
-					(top * 2 - 1 < 0 ? (top * 2 - 1) * -1 : top * 2 - 1)
+						// flip upside down if horizontal image is in the top of area
+						// if (top < 0.5) {
+						// 	rotationByPlace = rotationByPlace - 180
+						// }
+					}
 
-				newPositions[index] = {
-					rotation: rotationByPlace,
-					top: top * 120 - 10,
-					left: left * 120 - 10,
-					zIndex: Math.floor(fromCenter * 100),
-					blur: (fromCenter * -1 + 0.6) * 4,
-					brightness: fromCenter + 0.6,
-				}
-				return newPositions
-			})
-		}
+					const fromCenter =
+						(left * 2 - 1 < 0 ? (left * 2 - 1) * -1 : left * 2 - 1) *
+						(top * 2 - 1 < 0 ? (top * 2 - 1) * -1 : top * 2 - 1)
 
-		return (
-			//change to div with background insted of img
-			<img
-				src={item.url}
-				style={
-					positions[index]
-						? {
-								transform: `
+					newPositions[index] = {
+						rotation: rotationByPlace,
+						top: top * 120 - 10,
+						left: left * 120 - 10,
+						zIndex: Math.floor(fromCenter * 100),
+						blur: (fromCenter * -1 + 0.6) * 4,
+						brightness: fromCenter + 0.6,
+					}
+					return newPositions
+				})
+			}
+
+			return (
+				//change to div with background insted of img
+				<img
+					src={item.url}
+					style={
+						positions[index]
+							? {
+									transform: `
                                     translate(-50%, -50%)
 									scale(.6)
                                     rotate(${positions[index].rotation}deg)
                                 `,
-								top: `${positions[index].top}%`,
-								left: `${positions[index].left}%`,
-								zIndex: positions[index].zIndex,
-								filter: `blur(${positions[index].blur}px) 
+									top: `${positions[index].top}%`,
+									left: `${positions[index].left}%`,
+									zIndex: positions[index].zIndex,
+									filter: `blur(${positions[index].blur}px) 
 									drop-shadow(0 0 2rem black) 
 									brightness(${positions[index].brightness})`,
-						  }
-						: {}
+							  }
+							: {}
+					}
+				/>
+			)
+		})
+		return theImgs
+	}
+
+	useEffect(() => {
+		if (containerRef.current) {
+			const observer = new ResizeObserver((entries) => {
+				for (let entry of entries) {
+					if (entry.contentRect.width > 2560) {
+						setIsWide(true)
+					} else {
+						setIsWide(false)
+					}
 				}
-			/>
-		)
-	})
+			})
+
+			observer.observe(containerRef.current)
+
+			return () => {
+				observer.disconnect()
+			}
+		}
+	}, [])
+
+	let displayImgs = theImages()
+
+	useEffect(() => {
+		console.log(isWide)
+		displayImgs = theImages()
+	}, [isWide])
+
 	return (
-		<div className='maes-collage'>
+		<div className='maes-collage' ref={containerRef}>
 			<div
 				className='maes-collage-background'
 				style={{
