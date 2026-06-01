078eb67bb57eeba9c6ec1bb7900d0d0ffd19b1d4
Ina Eklund <theartflower@gmail.com>
2026-06-01 11:03:08 +0200

    git no longer ignores the build folder

---
+++ build/attachment_meta.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'cfcb8a41ef2c348400f4');
---
+++ build/blocks/maes_collage_banner/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'e9eeb269601c7e2cc413');
